aa8647840e049b4711100da0699207ac55a8d0d3
DESKTOP-PI164O8\Admin <pearmonday>
2021-06-02 22:53:09 +0700

    การบ้านครั้งที่6 ข้อที่2

---
+++ 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeWork6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Money ;
+            int Price;
+            Console.Write("InPut Price :");
+            Price = int.Parse(Console.ReadLine());
+            int Pay;
+            Console.Write("InPut Pay :");
+            Pay = int.Parse(Console.ReadLine());
+            int Change;
+            Change = Pay - Price  ;
+
+            Console.Write("My Money = {0}",Change);
+            Console.ReadLine();
+        }
+    }
+}
